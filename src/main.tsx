481e148838536ff2b8ad26e4c8a54238df9d6d19
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2023-04-04 22:18:35 +0300

    Move app.tsx to components

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import * as ReactDOMClient from "react-dom/client";
-import { App } from "./app";
+import { App } from "./components/app";
 
 const container = document.getElementById("app");
 const root = ReactDOMClient.createRoot(container);
