c3a81d9eb6938e83805ac10a0bc9c4a0eb468192
Bojidar Qnkov <bojidar.qnkov@gmail.com>
2023-04-06 22:13:59 +0300

    Replace let with const

--- src/services/flatpakService.ts
+++ src/services/flatpakService.ts
@@ -3,17 +3,17 @@ import { FlatpakApp } from "../models/flatpakApp";
 import * as commandHelpers from "../helpers/commandHelpers";
 
 export const getFlatpakVersion = async (): Promise<string> => {
-  let version = await commandHelpers.executeCommand("flatpak --version");
+  const version = await commandHelpers.executeCommand("flatpak --version");
   return version.replace("\n", "");
 };
 
 export const getInstalledApps = async (): Promise<FlatpakApp[]> => {
-  let result = await commandHelpers.executeCommand("flatpak list --app");
+  const result = await commandHelpers.executeCommand("flatpak list --app");
   return result
     .split("\n")
     .filter((line: string) => line.split("\t")[0].trim() !== "")
     .map((line: string) => {
-      let row = line.split("\t");
+      const row = line.split("\t");
       return new FlatpakApp(row[0], row[1], row[2], row[3], row[4], true);
     });
 };
